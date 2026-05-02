a3f1da3fe8d46d3f917a08d229c4f33498e45002
Javier Alonso <alonsojavizz@gmail.com>
2026-05-01 19:24:28 -0600

    Se inicio la plantilla de diseño

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,9 @@
 
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
-});
+// Route::get('/', function () {
+//     return view('welcome');
+// });
+
+
+route::get('/',[PageController::class, 'index'])->name('page.index');
