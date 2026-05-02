3333eb11d49b5022cd2e5ba7f7b9b81ff68ce1ac
Javier Alonso <alonsojavizz@gmail.com>
2026-05-01 19:36:29 -0600

    rutas

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\PagesController;
 use Illuminate\Support\Facades\Route;
 
 // Route::get('/', function () {
@@ -7,4 +8,4 @@ use Illuminate\Support\Facades\Route;
 // });
 
 
-route::get('/',[PageController::class, 'index'])->name('page.index');
+route::get('/', [PagesController::class, 'index'])->name('page.index');
