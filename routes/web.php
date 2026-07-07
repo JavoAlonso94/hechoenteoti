5258cd40c133b4d752e82df1249494f73bc75dbb
karenzamvega <karenzamveg@gmail.com>
2026-07-06 23:07:18 -0600

    feat(reservas): ruta POST /reservas

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\PagesController;
+use App\Http\Controllers\ReservaController;
 use Illuminate\Support\Facades\Route;
 
 // Route::get('/', function () {
@@ -9,3 +10,4 @@ use Illuminate\Support\Facades\Route;
 
 
 Route::get('/', [PagesController::class, 'index'])->name('page.index');
+Route::post('/reservas', [ReservaController::class, 'store'])->name('reservas.store');
